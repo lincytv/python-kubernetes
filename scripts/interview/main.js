338ba56771cd29b9e683fb5425f7ada7d9d3fee3
Lincy Varghese <lincytv@gmail.com>
2021-07-27 21:26:08 +0530

    Update main.js

--- scripts/interview/main.js
+++ scripts/interview/main.js
@@ -1,13 +1,15 @@
-const k8s = require('@kubernetes/client-node');
+const k8sApi = require('@kubernetes/client-node');
+const json = require('json');
+const kc = new k8sApi.KubeConfig();
+kc.loadFromDefault();
+const k8s = kc.makeApiClient(k8sApi.CoreV1Api);
+k8s.listNode().then((res) => {
+    for(var i in res.body.items) {
 
-const kconf = new k8s.KubeConfig();
-
-kconf.loadFromDefault();
-
-const k8sApi = kconf.makeApiClient(k8s.CoreV1Api);
-
-k8sApi.listNode().then(( res ) => {
- for i in k8sApi.listNode()
-   console.log(i.items)
-
+        if(res.body.items[i].status.conditions[4].status == "True"){
+            var node_status = "Ready"
+        }
+        console.log(res.body.items[i].metadata.name, res.body.items[i].metadata.creationTimestamp, res.body.items[i].status.addresses[1].address, node_status);
+    };
 });
+module.exports
